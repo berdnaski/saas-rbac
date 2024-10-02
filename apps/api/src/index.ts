79f079172d120132b595651e72ddaa7e549ad4b0
berdnaski <berdnaski1177@gmail.com>
2024-10-02 14:12:46 -0300

    feat(auth): configure role-based permissions with CASL

---
+++ apps/api/src/index.ts
@@ -0,0 +1,10 @@
+import { defineAbilityFor } from "@saas/auth";
+
+const ability = defineAbilityFor({ role: 'MEMBER'})
+
+const userCanInviteSomeoneElse = ability.can('invite', 'User');
+const userCanDeleteOtherUsers = ability.can('delete', 'User');
+
+const userCannotDeleteOtherUsers = ability.cannot('delete', 'User');
+
+console.log(userCanInviteSomeoneElse, userCanDeleteOtherUsers, userCannotDeleteOtherUsers);
---
+++ packages/auth/src/index.ts
@@ -0,0 +1,27 @@
+import { createMongoAbility, ForcedSubject, CreateAbility, MongoAbility, AbilityBuilder } from '@casl/ability';
+import { User } from './modes/user';
+import { permissions } from './permissions';
+
+const actions = ['manage', 'invite', 'delete'] as const;
+const subjects = ['User', 'all'] as const;
+type AppAbilities = [
+  typeof actions[number],
+  typeof subjects[number] | ForcedSubject<Exclude<typeof subjects[number], 'all'>>
+];
+
+export type AppAbility = MongoAbility<AppAbilities>;
+export const createAppAbility = createMongoAbility as CreateAbility<AppAbility>;
+
+export function defineAbilityFor(user: User) {
+  const builder = new AbilityBuilder(createAppAbility);
+
+  if (typeof permissions[user.role] !== 'function') {
+    throw new Error(`Permissions for role ${user.role} not found.`);
+  }
+
+  permissions[user.role](user, builder);
+
+  const ability = builder.build();
+
+  return ability;
+}
