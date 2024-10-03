f911c5ad2daad31ea6677d44745e7464ba3527da
berdnaski <berdnaski1177@gmail.com>
2024-10-03 10:24:33 -0300

    feat(auth): add automatic association via email

--- apps/api/src/http/routes/auth/create-account.ts
+++ apps/api/src/http/routes/auth/create-account.ts
@@ -26,16 +26,30 @@ export async function createAccount(app: FastifyInstance) {
             })
 
             if (userWithSameEmail) {
-                return reply.status(400).send({ message: 'user with same e-mail already exists.' })
+                return reply.status(400).send({ message: 'User with the same e-mail already exists.' });
             }
 
-            const passwordHash = await hash(password, 6)
+            const [, domain] = email.split('@');
+
+            const autoJoinOrganization = await prisma.organization.findFirst({
+                where: {
+                    domain,
+                    shouldAttachUsersByDomain: true,
+                }
+            })
+
+            const passwordHash = await hash(password, 6);
 
             await prisma.user.create({
                 data: {
                     name,
                     email,
                     passwordHash,
+                    member_on: autoJoinOrganization ? {
+                        create: {
+                            organizationId: autoJoinOrganization.id,
+                        },
+                    } : undefined,
                 },
             })
 
