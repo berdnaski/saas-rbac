87e8dde66f2bf9fa099ee4e9e3d4cdd2255e08cc
berdnaski <berdnaski1177@gmail.com>
2024-10-03 00:44:24 -0300

    feat(permissions): configure role-based access control using CASL

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -1,11 +1,28 @@
 import { createMongoAbility, CreateAbility, MongoAbility, AbilityBuilder } from '@casl/ability';
-import { User } from './modes/user';
+import { User } from './models/user';
 import { permissions } from './permissions';
-import { UserSubject } from './subjects/user';
-import { ProjectSubject } from './subjects/project';
+import { userSubject } from './subjects/user';
+import { projectSubject } from './subjects/project';
+import { z } from 'zod';
+import { organizationSubject } from './subjects/organization';
+import { inviteSubject } from './subjects/invite';
+import { billingSubject } from './subjects/billing';
 
-type AppAbilities = UserSubject | ProjectSubject | ['manage', 'all']
+export * from './models/organization'
+export * from './models/project'
+export * from './models/user'
 
+const appAbilitiesSchema = z.union([
+  projectSubject,
+  userSubject,
+  organizationSubject,
+  inviteSubject,
+  billingSubject,
+
+  z.tuple([z.literal('manage'),z.literal('all')])
+])
+
+type AppAbilities = z.infer<typeof appAbilitiesSchema>
 
 export type AppAbility = MongoAbility<AppAbilities>;
 export const createAppAbility = createMongoAbility as CreateAbility<AppAbility>;
@@ -19,7 +36,11 @@ export function defineAbilityFor(user: User) {
 
   permissions[user.role](user, builder);
 
-  const ability = builder.build();
+  const ability = builder.build({
+    detectSubjectType(subject) {
+      return subject.__typename
+    },
+  });
 
   return ability;
 }
--- packages/auth/src/permissions.ts
+++ packages/auth/src/permissions.ts
@@ -1,9 +1,8 @@
 import { AbilityBuilder } from "@casl/ability"
 
 import { AppAbility } from "."
-import { User } from "./modes/user"
-
-type Role = 'ADMIN' | 'MEMBER'
+import { User } from "./models/user"
+import { Role } from "./roles"
 
 type PermissionsByRole = (
     user: User, 
@@ -12,11 +11,18 @@ type PermissionsByRole = (
 
 export const permissions: Record<Role, PermissionsByRole> = {
 
-    ADMIN(_, { can }) {
-        can('manage', 'all')
+    ADMIN(user, { can, cannot }) {
+        can('manage', 'all');
+
+        cannot(['transfer_ownership', 'update'], 'Organization')
+        can(['transfer_ownership', 'update'], 'Organization', { ownerId: { $eq: user.id } })
     },
-    MEMBER(_, { can }) {
-        can('invite', 'User')
-        can('manage', 'Project')
+    MEMBER(user, { can }) {
+        can('get', 'User')
+        can(['create', 'get'], 'Project')
+        can(['update', 'delete'], 'Project', { ownerId: { $eq: user.id}})
+    },
+    BILLING(_, { can }) {
+        can('manage', 'Billing')
     },
 }
