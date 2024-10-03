bd3a39c9f18c9c7d48a775cbd4046a35449b1403
berdnaski <berdnaski1177@gmail.com>
2024-10-03 10:14:14 -0300

    docs(api): add swagger for API documentation

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -1,12 +1,30 @@
 import fastify from "fastify";
+import fastifySwagger from '@fastify/swagger';
+import fastifySwaggerUI from '@fastify/swagger-ui';
 import fastifyCors from "@fastify/cors";
-import { serializerCompiler, validatorCompiler, ZodTypeProvider } from "fastify-type-provider-zod";
+import { jsonSchemaTransform, serializerCompiler, validatorCompiler, ZodTypeProvider } from "fastify-type-provider-zod";
 import { createAccount } from "./routes/auth/create-account";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
 app.setSerializerCompiler(serializerCompiler);
 app.setValidatorCompiler(validatorCompiler);
+
+app.register(fastifySwagger, {
+    openapi: {
+      info: {
+        title: 'Next.js SaaS',
+        description: 'Full-stack SaaS app with multi-tenant & RBAC.',
+        version: '1.0.0',
+      },
+      servers: [],
+    },
+    transform: jsonSchemaTransform,
+  });
+
+app.register(fastifySwaggerUI, {
+    routePrefix: '/docs',
+})
 
 app.register(fastifyCors);
 
